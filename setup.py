98f23e52490fa8acdf69aa9faa70b45a34d192c8
melug <sw06d103@gmail.com>
2010-12-10 10:37:03 +0800

    SetupTools ашиглан хэрэглэхэд хялбархан болгов.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name = 'Music Player',
+    version = '0.1',
+    author = 'Sharavsambuu',
+    description = 'Music Player',
+    packages = find_packages(),
+    install_requires = [ ],
+    entry_points = {
+            'console_scripts' : [
+                'shmusic = musicplayer:main',
+            ]
+        }
+    )
