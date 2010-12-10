f0a817ac3267d00b2ea8490111c8de39a6800a99
melug <sw06d103@gmail.com>
2010-12-10 11:51:02 +0800

    аргументийг шалгадаг болов. хоосон функцүүдийг хэрэгжүүлэх хэрэгтэй.

--- musicplayer/__init__.py
+++ musicplayer/__init__.py
@@ -1,2 +1,36 @@
+import sys
+
 def main():
-    import musicplayer
+    if len(sys.argv) == 1:
+        showHelp()
+    elif len(sys.argv) == 2:
+        opt = sys.argv[1]
+        if opt == '-l':
+            showList()
+        elif opt.startswith('http:'):
+            playUri(opt)
+        else:
+            showHelp()
+    elif len(sys.argv) == 3:
+        opt = sys.argv[1]
+        if opt == '-p':
+            uriNumber = int(sys.argv[2])
+            playNumber(uriNumber)
+        else:
+            showHelp()
+    else:
+        showHelp()
+
+def showHelp():
+    print '''
+    shmusic [ stream_uri | -h | -l | -p number ]
+    '''
+
+def playUri(uri):
+    print 'playing uri', uri
+
+def playNumber(number):
+    print 'playing #', number
+
+def showList():
+    print 'showing list'
